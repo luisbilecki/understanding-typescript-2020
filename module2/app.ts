40f25fa0c632d2c8c5078038239447b7c320d106
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-08-10 22:22:14 -0300

    19. Working with Tuples

--- module2/app.ts
+++ module2/app.ts
@@ -1,12 +1,20 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[],
+  role: [number, string]
+} = {
+// const person = {
   name: 'Maximilian',
   age: 30,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
 }
+
+// person.role.push('admin')
+// person.role[1] = 10
+
+person.role = [0, 'admin']
 
 let favoriteActivities: string[]
 // let favoriteActivities: any[]
