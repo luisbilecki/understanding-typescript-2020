48741ab7cebb3b825aea835197283ce930862237
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-08-10 22:35:50 -0300

    20. Working with Enums

--- module2/app.ts
+++ module2/app.ts
@@ -1,20 +1,28 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[],
-  role: [number, string]
-} = {
-// const person = {
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+// Enum is the first custom type
+enum Role  { ADMIN = 5, READ_ONLY = 100, AUTHOR = 201 } // an string is accepted as value too
+
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[],
+//   role: [number, string]
+// } = {
+const person = {
   name: 'Maximilian',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.ADMIN
 }
 
 // person.role.push('admin')
 // person.role[1] = 10
 
-person.role = [0, 'admin']
+// person.role = [0, 'admin']
 
 let favoriteActivities: string[]
 // let favoriteActivities: any[]
@@ -24,4 +32,8 @@ console.log(person.name)
 
 for (const hobby of person.hobbies) {
   console.log(hobby, hobby.toUpperCase())
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('is admin')
 }
