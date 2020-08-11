e2d3698cdc83178261122b31a74b9167332c5ee7
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-08-10 22:14:53 -0300

    18. Arrays Types

--- module2/app.ts
+++ module2/app.ts
@@ -1,10 +1,19 @@
-const person: {
-  name: string;
-  age: number;
-} = {
-// const person = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
   name: 'Maximilian',
-  age: 30
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
 }
 
+let favoriteActivities: string[]
+// let favoriteActivities: any[]
+favoriteActivities = ['Sports']
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+  console.log(hobby, hobby.toUpperCase())
+}
