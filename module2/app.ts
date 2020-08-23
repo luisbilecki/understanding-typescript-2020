2f8bee289fee7ac38e84da95875be936312ccae0
Luis F. Bilecki <luis.bilecki@gmail.com>
2020-08-22 22:55:57 -0300

    27. Functions as Types

--- module2/app.ts
+++ module2/app.ts
@@ -7,3 +7,9 @@ function printResult (num: number): void {
 }
 
 printResult(add(6, 5))
+
+let combineValues: (a: number, b: number) => number
+
+combineValues = add
+
+console.log(combineValues(8, 8))
