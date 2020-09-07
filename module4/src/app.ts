bd2aada932462f17a803e2489ae8ff87e4d4240a
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-09-06 21:18:09 -0300

    50. Arrow Functions

--- module4/src/app.ts
+++ module4/src/app.ts
@@ -5,10 +5,10 @@ let age = 30
 age = 29
 
 var result
-function add (a: number, b: number) {
-  result = a + b
-  return result
-}
+// function add (a: number, b: number) {
+//   result = a + b
+//   return result
+// }
 
 if (age > 20) {
   let isOld = true
@@ -18,3 +18,15 @@ if (age > 20) {
 // console.log(isOld)
 
 // console.log(add(3, 4))
+
+const add = (a: number, b: number) => a + b;
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', event => console.log(event));
+}
+
+printOutput(add(5, 2));
