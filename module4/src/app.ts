dbfb79f195bd60915cdb25c3db5a14e4abd2290e
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-09-06 21:07:30 -0300

    49. "let" and "const"

---
+++ module4/src/app.ts
@@ -0,0 +1,20 @@
+const userName = 'Luis'
+// userName = 'Luis Bilecki'
+let age = 30
+
+age = 29
+
+var result
+function add (a: number, b: number) {
+  result = a + b
+  return result
+}
+
+if (age > 20) {
+  let isOld = true
+  console.log(isOld)
+}
+
+// console.log(isOld)
+
+// console.log(add(3, 4))
