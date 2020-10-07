d84892c02619711f25ec75e0ab254edb407512cc
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-10-07 11:21:14 -0300

    114. Example: Creating an "Autobind" Decorator

--- module8/src/app.ts
+++ module8/src/app.ts
@@ -99,3 +99,33 @@ class Product {
 // Decorators not run at runtime... Only when class is defined
 const p1 = new Product('Book 1', 10);
 const p2 = new Product('Book 2', 10);
+
+function Autobind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = 'This works!';
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
