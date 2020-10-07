24f1110f640cb3610f4b49fef93bf66ab8ac9938
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-10-07 16:44:35 -0300

    125. Fetching User Input

--- module9/src/app.ts
+++ module9/src/app.ts
@@ -50,10 +50,41 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    const isEmptyTitle = enteredTitle.trim().length === 0;
+    const isEmptyDescription = enteredDescription.trim().length === 0;
+    const isEmptyPeople = enteredPeople.trim().length === 0;
+
+    if (isEmptyTitle || isEmptyDescription || isEmptyPeople) {
+      alert('Invalid input, please try again!');
+      return;
+    }
+
+    return [enteredTitle, enteredDescription, +enteredPeople];
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
   @Autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title);
+      console.log(description);
+      console.log(people);
+      this.clearInputs();
+    }
   }
 
   private configure() {
