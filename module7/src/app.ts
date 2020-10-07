3109256676992508c957b6e5ca9b76f62038c5f5
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-10-07 09:30:22 -0300

    101. Generic Utility Types

--- module7/src/app.ts
+++ module7/src/app.ts
@@ -83,3 +83,26 @@ const numberStorage = new DataStorage<number>();
 // objStorage.addItem({ name: 'Manu' });
 // objStorage.removeItem(maxObj);
 // console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Max', 'Luis'];
+// names.push('Manu');
+// names.pop();
