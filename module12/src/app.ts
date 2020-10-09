d63c1d8de78406e7fa2d0bee98ba425c36c3568c
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-10-09 16:16:09 -0300

    163. No Types Needed: class-transformer

--- module12/src/app.ts
+++ module12/src/app.ts
@@ -1,7 +1,23 @@
 import _ from 'lodash';
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
 
 declare var GLOBAL: any;
 
 console.log(_.shuffle([1, 2, 3]));
 
 console.log(GLOBAL);
+
+import { Product } from './product.model';
+
+const products = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: 'A Book', price: 10.99 },
+];
+
+// const p1 = new Product('A Book', 12.99);
+// console.log(p1.getInformation());
+
+const loadedProducts = plainToClass(Product, products);
+
+loadedProducts.forEach(product => console.log(product.getInformation()));
