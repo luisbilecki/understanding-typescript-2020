64600a168c49dabcec0c6b3b402c872c55af382a
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-10-09 15:09:49 -0300

    143. Organizing Files & Folders

---
+++ module10/src/util/validation.ts
@@ -0,0 +1,46 @@
+namespace App {
+  // Validation logic
+  export interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+  }
+
+  export function validate(validatableInput: Validatable) {
+    const {
+      value,
+      required,
+      minLength,
+      maxLength,
+      min,
+      max,
+    } = validatableInput;
+
+    let isValid = true;
+
+    if (required) {
+      isValid = isValid && value.toString().trim().length !== 0;
+    }
+
+    if (minLength && typeof value === 'string') {
+      isValid = isValid && value.length >= minLength;
+    }
+
+    if (maxLength && typeof value === 'string') {
+      isValid = isValid && value.length <= maxLength;
+    }
+
+    if (min && typeof value === 'number') {
+      isValid = isValid && value >= min;
+    }
+
+    if (max && typeof value === 'number') {
+      isValid = isValid && value <= max;
+    }
+
+    return isValid;
+  }
+}
