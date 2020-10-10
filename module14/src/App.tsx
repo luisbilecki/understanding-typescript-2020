9850d7ce61feaf982dde2593ce3461318ea1ca53
Luis Felipe Bilecki <luis.bilecki@gmail.com>
2020-10-10 12:31:11 -0300

    182. Managing State Better

--- module14/src/App.tsx
+++ module14/src/App.tsx
@@ -5,10 +5,13 @@ import NewTodo from './components/NewTodo';
 import { Todo } from './todo.model';
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState<Todo>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    setTodos([{ id: Math.random().toString(), text }]);
+    setTodos(prevTodos => [
+      ...prevTodos,
+      { id: Math.random().toString(), text },
+    ]);
   };
 
   return (
